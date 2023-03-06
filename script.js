89269348f13f980307dbf1c2002db66a68be3ceb
Igor Flores <91649183+Nikaiy@users.noreply.github.com>
2023-03-06 20:56:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNav () {
+    let div = document.getElementById("showmenu");
+    if (div.style.display == "none") {
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none"
+    }
+}
